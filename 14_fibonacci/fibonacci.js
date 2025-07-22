83ab167cd535c695dfc522a890902bde8d0f28b7
B-Beetroot <216270728+B-Beetroot@users.noreply.github.com>
2025-07-23 01:11:14 +0200

    Done 13-16 js-exercises

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num<0) return "OOPS";
+    const arrFibonacci = [0, 1];
+    for (let i = 2; i <= num; i++) {
+        const f = arrFibonacci[i - 1] + arrFibonacci[i - 2];
+        arrFibonacci.push(f);
+    }
+    return arrFibonacci[num];
 };
 
 // Do not edit below this line
--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+return people.reduce((oldest, person) => {
+    const currentYear = new Date().getFullYear();
+    let currentAge;
+    if (!person.yearOfDeath) { 
+      currentAge = currentYear - person.yearOfBirth;
+    } else {
+      currentAge = person.yearOfDeath - person.yearOfBirth;
+    }
+    const oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+    return currentAge > oldestAge ? person : oldest;
+  });
 };
 
 // Do not edit below this line
