ed60ac34a887dd5a0272774f621a330f5d9e0532
B-Beetroot <216270728+B-Beetroot@users.noreply.github.com>
2025-07-22 22:31:35 +0200

    Done 12th js-exercise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,53 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  return a-b;
+};
+/*
+const sum = function(arr) {
+  let sum = 0;
+  if (arr.length>0) {
+  for (i=0;i<arr.length;i++){
+	 sum += arr[i];
+  }
+  return sum;
+  } else {
+    return 0;
+  }
+};
+*/
+const sum = function(arr) {
+  return arr.reduce((total, current) => total + current, 0);
+};
+/*
+const multiply = function(arr) {
+   let result=1;
+  if (arr.length>0) {
+  for (i=0;i<arr.length;i++){
+	 result = result*arr[i];
+  }
+  return result;
+  } else {
+    return 0;
+  }
+};
+*/
+const multiply = function(arr) {
+  return arr.reduce((total, current) => total * current);
 };
 
-const sum = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(n) {
+ let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
